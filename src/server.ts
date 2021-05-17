b9dbab45cf303ad4cbaf0b534b29b8a01626470c
suriya.t <suriya.t@payoda.com>
2021-05-17 09:42:29 +0530

    swagger files added

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,36 @@
 import App from './app'
 import * as bodyParser from 'body-parser'
 import loggerMiddleware from './middleware/logger'
+import * as express from 'express'
+
+import * as YAML from 'yamljs';
+import * as swaggerUi from 'swagger-ui-express';
 
 import HomeRoute from './routes/home/home.route'
 import AuthRoute from 'routes/auth/auth.route'
 import authenticateToken from 'middleware/authentication'
+
+const server: express.Application = express();
+
+const options = {
+    swaggerOptions: {
+      supportedSubmitMethods: []
+    }
+  };
+
+const swaggerDocument = YAML.load('docs/swagger.yaml');
+
+server.use(
+    '/api-docs',
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocument, options)
+  );
+  
+  server.all('/swagger.json', (req, res) => {
+    console.info(`swagger document called`);
+  
+    return res.json(swaggerDocument);
+  });
 
 
 const app = new App({
