13423a4fe02e77aab20a8ad6a981f0472342f061
suriya.t <suriya.t@payoda.com>
2021-05-25 16:24:27 +0530

    changes updated

---
+++ src/service/send.mail.ts
@@ -0,0 +1,39 @@
+import * as nodemailer from 'nodemailer';
+
+
+class EmailService {
+  public sendMail = (mailOptions) => {
+    try {
+     
+      const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'alsirajmailer@gmail.com',
+          pass: 'Alsiraj@mailer2021'
+        }
+      });
+
+      console.log('mailOptions ===', mailOptions)
+    
+
+      transporter.sendMail(mailOptions, (error: any, info: any) => {
+        if (error) {
+          console.error(`Email failed and the reson is :  ${error.message}`);
+        } else {
+
+
+          console.info(`Email send successfully:  ${info}`);
+
+          console.debug(`Email Content`, mailOptions);
+        }
+
+        return `Email sent ${info}`;
+      });
+    }
+    catch (error) {
+      throw error;
+    }
+  };
+}
+
+export default EmailService;
